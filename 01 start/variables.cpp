2631795985c707d4c7ed154a438fcdd514229e9e
Sandun <vitharana1996@gmail.com>
2025-12-23 02:11:18 -0600

    variables

---
+++ 01 start/variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+
+    int age = 45;
+    double temp = 29.888;
+
+    std::cout<< "Age :"<<age <<std::endl;
+    std::cout<< "Temperature :"<<temp<<std::endl;
+    return 0;
+}
