3d48c0a1d5bfe3b3952253fb23290a618293811f
Sandun <vitharana1996@gmail.com>
2025-12-23 02:27:36 -0600

    constant variables

---
+++ 01 start/variables2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(){
+
+    // boolean var  true / false
+
+    bool isMarried = true;
+    std::cout<<isMarried<<std::endl;
+
+    // string variables
+
+    std::string name = "sandun";
+    std::cout<<name<<std::endl;
+
+
+    return 0;
+}
---
+++ 01 start/variables3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+
+    const int maxValue = 500;
+    // maxValue = 899; // not allowed
+
+    std::cout<<maxValue<<std::endl;
+    return 0;
+}
